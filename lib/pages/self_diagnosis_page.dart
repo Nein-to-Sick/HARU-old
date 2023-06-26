7df2965592831e20fcc80100b991b717e358acf7
markFT39 <43339545+markFT39@users.noreply.github.com>
2023-06-26 15:54:03 +0900

    self diagnosis page update

---
+++ lib/pages/self_diagnosis_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class SelfDiagnosisPage extends StatelessWidget {
+  const SelfDiagnosisPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(),
+    );
+  }
+}
