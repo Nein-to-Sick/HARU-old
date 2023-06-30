ff403bec2586ebcf882f64975ab516f26351de2e
Sehan Kwon <42161023+sehan2709-cmis@users.noreply.github.com>
2023-06-30 15:44:01 +0900

    profile page 수정

--- lib/pages/mainPage/profile.dart
+++ lib/pages/mainPage/profile.dart
@@ -1,10 +1,25 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
-class Profile extends StatelessWidget {
-  const Profile({Key? key}) : super(key: key);
+import '../../auth/auth.dart';
+
+class ProfilePage extends StatelessWidget {
+  const ProfilePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    void signUserOut() {
+      FirebaseAuth.instance.signOut();
+      Navigator.push(
+          context, MaterialPageRoute(builder: (context) => AuthPage()));
+    }
+
+    return Scaffold(
+      appBar: AppBar(title: Text("Profile Page"), actions: [
+        IconButton(onPressed: signUserOut, icon: Icon(Icons.logout))
+      ]),
+      body: Text("hello world"),
+    );
   }
 }
