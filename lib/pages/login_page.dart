de3b5b65f6a7df1bd0d9ccb2c997c19c84d07a5d
Sehan Kwon <42161023+sehan2709-cmis@users.noreply.github.com>
2023-06-30 15:43:12 +0900

    카카오 애플 로그인 1.0에 됩니다

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -164,9 +164,13 @@ class _LoginPageState extends State<LoginPage> {
                             onTap: () => AuthService().signInWithGoogle(),
                             imagePath: 'assets/googlelogin.jpg'),
                         SquareTile(
-                            onTap: () {}, imagePath: 'assets/kakaologin.jpg'),
+                            onTap: () {
+                              showErrorMessage("카카오톡 로그인은 v.1.0에 추가될 예정입니다.");
+                            }, imagePath: 'assets/kakaologin.jpg'),
                         SquareTile(
-                            onTap: () {}, imagePath: 'assets/applelogin.jpg'),
+                            onTap: () {
+                              showErrorMessage("애플 로그인은 v.1.0에 추가될 예정입니다.");
+                            }, imagePath: 'assets/applelogin.jpg'),
                       ],
                     ),
                   ],
